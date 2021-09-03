4b1c76a7626ca69f2e3f6ae51ef9210ffb508238
NaojiroUeno <u395208e@ecs.osaka-u.ac.jp>
2021-09-04 00:29:11 +0900

    サイトの名前をターミナルに表示するように変更

---
+++ web.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys as keys
+
+# windowsの場合 (Cドライブ直下にchromedriver.exeがある前提)
+driver = webdriver.Chrome(executable_path="C:\chromedriver.exe") # Macの方はこの行をコメントアウト
+
+### 1.Webサイトにアクセスする
+driver.get("https://aiacademy.jp/")
+
+print(driver.title)
